9313d447e0db0f17242ece7db2a8303352f80b3a
georgeanthony33 <georgeanthony33@gmail.com>
2020-03-14 14:23:52 +0000

    Ready for deployment

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -27,57 +27,67 @@ class Home extends React.Component {
 
   render() {
     return (
-      <>
-        <section className="hero is-fullheight">
-          <div className="hero-body home-page-background">
-            <div className="container home-page is-paddingless is-marginless">
-              <div className="columns is-centered">
-                <form className="column is-half" onSubmit={this.handleSubmit}>
+      <div className="homepage-outer-container">
 
-                  <div className="columns is-centered">
-                    <div className="column">
-                      <div className="field">
-                        <div className="control">
-                          <input className="input is-rounded event-input is-primary" type="text" name="searchWord" value={this.state.searchValues.searchWord} onChange={this.handleChange} placeholder="Find your event" />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+        <div className="color-overlay"></div>
 
-                  <div className="columns is-centered">
-                    <div className="column is-half">
-                      <div className="field">
-                        <div className="control">
-                          <input className="input is-rounded is-primary" type="date" name="searchDate" value={this.state.searchValues.searchDate} onChange={this.handleChange} />
-                        </div>
-                      </div>
-                    </div>
-                    <div className="column is-half">
-                      <div className="field">
-                        <div className="control">
-                          <input className="input is-rounded is-primary" type="text" name="searchLocation" value={this.state.searchValues.searchLocation} onChange={this.handleChange} placeholder="Anywhere"/>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                    
-                  <div className="columns is-centered">
-                    <div className="column is-half">
-                      <div className="field">
-                        <div className="control">
-                          <button type="submit" className="button is-fullwidth is-primary is-size-5">Search</button>
-                        </div>
-                      </div>
-                    </div>
-
-                  </div>
-                </form>
-              </div>
+        <div className="homepage-inner-container">
+          <div className="title-outer-container">
+            <div className="title-inner-container">
+              <h1 className="home-title">FunFindr</h1>
+              <br />
+              <h4 className="title is-3 has-text-white has-text-centered home-subtitle">Search for your favourite events, any time, any place!</h4>
             </div>
           </div>
-          <div className="color-overlay"></div>
-        </section>
-      </>
+
+          <div className="search-outer-container">
+            <div className="columns is-centered">
+              
+              <form className="column" onSubmit={this.handleSubmit}>
+
+                <div className="columns is-centered">
+                  <div className="column">
+                    <div className="field">
+                      <div className="control">
+                        <input className="input is-rounded event-input is-primary" type="text" name="searchWord" value={this.state.searchValues.searchWord} onChange={this.handleChange} placeholder="Find your event" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="columns is-centered">
+                  <div className="column is-half">
+                    <div className="field">
+                      <div className="control">
+                        <input className="input is-rounded is-primary" type="date" name="searchDate" value={this.state.searchValues.searchDate} onChange={this.handleChange} />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="column is-half">
+                    <div className="field">
+                      <div className="control">
+                        <input className="input is-rounded is-primary" type="text" name="searchLocation" value={this.state.searchValues.searchLocation} onChange={this.handleChange} placeholder="Anywhere"/>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                  
+                <div className="columns is-centered">
+                  <div className="column is-half">
+                    <div className="field">
+                      <div className="control">
+                        <button type="submit" className="button is-fullwidth is-primary is-size-5">Search</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+              </form>
+            </div>
+            
+          </div>
+        </div>
+      </div>
     )
   }
  
